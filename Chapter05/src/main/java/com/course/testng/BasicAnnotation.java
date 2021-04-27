0d620c0a5df7b2e57d4550d7cc5679bb320bd65e
943958916@qq.com <Lxd18170422759>
2021-04-27 10:41:20 +0800

    提交创建testng框架搭建成功代码

---
+++ Chapter05/src/main/java/com/course/testng/BasicAnnotation.java
@@ -0,0 +1,18 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * @auther jingsi
+ * @date Create on 2021/4/2710:17
+ * @hotkeys fori-for循环；sout-控制台输出；psvm-main函数
+ */
+
+public class BasicAnnotation {
+
+    @Test
+    public void testcase(){
+        System.out.println("welcome to from you!TestNg");
+    }
+}
+
