fdef20d9faddf5edeaaa2493016c0c102d7aec06
943958916@qq.com <Lxd18170422759>
2021-04-28 22:12:15 +0800

    *

---
+++ Chapter05/src/main/java/com/course/testng/multThread/MultiThreadOnAnnotion.java
@@ -0,0 +1,16 @@
+package com.course.testng.multThread;
+
+/**
+ * @auther jingsi
+ * @date Create on 2021/4/2716:49
+ * @hotkeys fori-for循环；sout-控制台输出；psvm-main函数
+ */
+
+public class MultiThreadOnAnnotion {
+
+    public void test(){
+        System.out.println(1);
+        System.out.printf("Thread Id:%s%n",Thread.currentThread());
+    }
+}
+
---
+++ Chapter05/src/main/java/com/course/testng/multThread/MultiThreadOnXml.java
@@ -0,0 +1,15 @@
+package com.course.testng.multThread;
+
+/**
+ * @auther jingsi
+ * @date Create on 2021/4/2716:47
+ * @hotkeys fori-for循环；sout-控制台输出；psvm-main函数
+ */
+
+public class MultiThreadOnXml {
+
+    public void test1(){
+        System.out.println(1);
+    }
+}
+
